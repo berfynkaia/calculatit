86cedef12c9267b6270c5f641df7d4f14a4f5931
berfin <k.berfyn@gmail.com>
2023-04-11 23:29:49 +0300

    starter

---
+++ main.py
@@ -0,0 +1,11 @@
+first_number = input("enter the first number:")
+operation = input("enter sign of the operation here:")
+second_number = input("enter the second number here:")
+if operation == "+":
+    print(int(first_number)+ int(second_number))
+if operation == "-" :
+    print(int(first_number)- int(second_number))
+if operation == "*" :
+    print(int(first_number) * int(second_number))
+if operation == "/" :
+    print(int(first_number) / int(second_number))
